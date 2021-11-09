0c6ec3e0c875156ec2d6588629da5d4c5ae693fe
AndersonS <klaito.b@gmail.com>
2021-11-09 14:22:11 -0300

    Create API TranQly

---
+++ server.js
@@ -0,0 +1,23 @@
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const cors = require("cors");
+const port = process.env.PORT || 3001;
+
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+
+app.db = router.db;
+
+const rules = auth.rewriter({
+  users:600, 
+  calendarUser:660, /*usuário/paciente precisa estar logado para ler e editar seu calendário*/
+  calendarProfessional:664, /*usuário/paciente precisa estar logado para agendar um horario com o profissional, todos podem visualizar o calendário*/
+  coments:644
+  
+});
+
+app.use(cors());
+app.use(rules);
+app.use(auth);
+app.use(router);
+app.listen(port);
