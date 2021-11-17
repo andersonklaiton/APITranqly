e85427ba99c19971b52a260ab1348d18ad84ac62
Anderson Silva <klaito.b@gmail.com>
2021-11-17 10:00:40 -0300

    change API

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 const cors = require("cors");
 const port = process.env.PORT || 3001;
-​
+
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
-​
+
 app.db = router.db;
-​
+
 const rules = auth.rewriter({
   users: 664,
   profissional: 660,
@@ -15,11 +15,11 @@ const rules = auth.rewriter({
   comments: 664,
   commentsPage: 660,
 });
-​
+
 app.use(cors());
 app.use(rules);
 app.use(auth);
 app.use(router);
 app.listen(port);
-​
+
 console.log("Server is running on port:", port);
