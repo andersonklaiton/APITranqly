ccb00c88533b9d0512333b77e2603d1e2dee388f
Anderson Silva <klaito.b@gmail.com>
2021-11-17 08:36:24 -0300

    refactored API

--- server.js
+++ server.js
@@ -2,23 +2,24 @@ const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 const cors = require("cors");
 const port = process.env.PORT || 3001;
-
+​
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
-
+​
 app.db = router.db;
-
+​
 const rules = auth.rewriter({
-  users:644, 
-  calendarUser:660, 
-  calendarProfessional:664, 
-  coments:644,
-  qualification:644
-  
+  users: 664,
+  profissional: 660,
+  patient: 660,
+  comments: 664,
+  commentsPage: 660,
 });
-
+​
 app.use(cors());
 app.use(rules);
 app.use(auth);
 app.use(router);
 app.listen(port);
+​
+console.log("Server is running on port:", port);
